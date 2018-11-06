3b8a5a8fd745e8a8a929b3e30bde3c9467d8ad07
lrn-white <liurennan12138@gmail.com>
2018-11-06 13:46:32 +0800

    提交通用配置文件

--- userserver/src/main/java/com/liu/userserver/UserserverApplication.java
+++ userserver/src/main/java/com/liu/userserver/UserserverApplication.java
@@ -3,6 +3,7 @@ package com.liu.userserver;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
