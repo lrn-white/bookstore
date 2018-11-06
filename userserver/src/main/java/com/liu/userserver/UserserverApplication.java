655c15ef3becef8e5d0105969ac7b80b7778a4c8
lrn-white <liurennan12138@gmail.com>
2018-11-06 10:56:59 +0800

    提交通用配置文件

--- userserver/src/main/java/com/liu/userserver/UserserverApplication.java
+++ userserver/src/main/java/com/liu/userserver/UserserverApplication.java
@@ -3,8 +3,11 @@ package com.liu.userserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
+@EnableConfigServer
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
