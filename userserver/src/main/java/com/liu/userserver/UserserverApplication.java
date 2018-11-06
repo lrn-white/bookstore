6ed754a721d19e5aee1570509a80709a311adf54
lrn-white <liurennan12138@gmail.com>
2018-11-06 11:23:38 +0800

    提交通用配置文件

--- userserver/src/main/java/com/liu/userserver/UserserverApplication.java
+++ userserver/src/main/java/com/liu/userserver/UserserverApplication.java
@@ -1,19 +1,33 @@
 package com.liu.userserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @EnableConfigServer
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
+@RestController
+@RefreshScope
 public class UserserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserserverApplication.class, args);
+	}
+
+	@Value("${foo}")
+	String foo;
+
+	@RequestMapping(value = "/hi")
+	public String hi(){
+		return foo;
 	}
 }
