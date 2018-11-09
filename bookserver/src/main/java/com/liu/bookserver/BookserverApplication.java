a0741b94ed6121123fe1b0129411801b8f721958
lrn-white <liurennan12138@gmail.com>
2018-11-09 14:54:13 +0800

    修改配置中心

--- bookserver/src/main/java/com/liu/bookserver/BookserverApplication.java
+++ bookserver/src/main/java/com/liu/bookserver/BookserverApplication.java
@@ -3,11 +3,13 @@ package com.liu.bookserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
+@RefreshScope
 public class BookserverApplication {
 
 	public static void main(String[] args) {
--- userserver/src/main/java/com/liu/userserver/UserserverApplication.java
+++ userserver/src/main/java/com/liu/userserver/UserserverApplication.java
@@ -1,31 +1,19 @@
 package com.liu.userserver;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
-@RestController
 @RefreshScope
 public class UserserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserserverApplication.class, args);
-	}
-
-	@Value("${test}")
-	String test;
-
-	@RequestMapping(value = "/hi")
-	public String hi(){
-		return test;
 	}
 }
