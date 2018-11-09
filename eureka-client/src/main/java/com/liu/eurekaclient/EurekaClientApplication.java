a5657acb28b15d88a85f0e38fa489eec2cd6ad93
lrn-white <liurennan12138@gmail.com>
2018-11-09 10:05:51 +0800

    上传配置文件

--- eureka-client/src/main/java/com/liu/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/liu/eurekaclient/EurekaClientApplication.java
@@ -2,10 +2,12 @@ package com.liu.eurekaclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
+@EnableConfigServer
 public class EurekaClientApplication {
 
 	public static void main(String[] args) {
